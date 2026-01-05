5ae573605686b3e8469fb9a8607308b26a126854
bruclares <clares.bruna@gmail.com>
2026-01-05 14:17:43 -0300

    chore(db): evolui Base com tablename automático e __repr__ padrão

--- app/db/base_class.py
+++ app/db/base_class.py
@@ -1,5 +1,22 @@
 from typing import Any
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, declared_attr
 
 class Base(DeclarativeBase):
-    pass
+    id: Any
+    __name__: str
+
+    #gera __tablename__ automaticamente pelo nome da classe
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+
+    # __repr__ para todos os models
+    def __repr__(self):
+        cols = []
+        # tenta pegar as colunas na tabela
+        if hasattr(self, "__table__"):
+            for col in self.__table__.columns.keys():
+                # filtra só o que interessa 
+                if col in ['id', 'email', 'is_active', 'title', 'full_name']:
+                    cols.append(f"{col}={getattr(self, col)}")
+        return f"<{self.__class__.__name__}({', '.join(cols)})>"
